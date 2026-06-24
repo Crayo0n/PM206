6cb337760f84ffa055ee3d8297eac2c8d7c6ccb3
Mauricio <mauricio28rd@gmail.com>
2026-06-23 18:45:36 -0600

    Practica 11. Pressable y Switch

--- Practica5/miapp2/screens/PressableScreen.js
+++ Practica5/miapp2/screens/PressableScreen.js
@@ -1,24 +1,87 @@
-/*Zona1: Importaciones de componentes y archivos*/
+// Zona 1 - Importaciones de componentes y archivos
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { Pressable, View, StyleSheet, Text, Switch } from "react-native";
+import { useState } from "react";
 
-/*Zona2: Main - Hogar de los componentes */
+// Zona 2 - Main - Hogar de los componentes
 export default function PressableScreen() {
+
+  const [buttonText, setButtonText] = useState("Dame Clic");
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <View style={styles.container}>
-        <Text> Aqui va la practica de Miguel</Text>
+    <View style={[styles.container, { backgroundColor: isDarkMode ? "#000000ff" : "#FFFFFF"  }]}>
+
+      <Pressable
+        style={styles.button}
+
+        onPress={() => {
+          console.log("Se presionó el botón");
+          setButtonText("Botón presionado");
+        }}
+
+        onPressIn={() => {
+          console.log("Se acaba de presionar el botón");
+        }}
+
+        onPressOut={() => {
+          console.log("Se acaba de soltar el botón");
+        }}
+
+        onLongPress={() => {
+          console.log("Presión prolongada");
+          setButtonText("Presión prolongada detectada");
+        }}
+      >
+        <Text style={styles.buttonText}>
+          {buttonText}
+        </Text>
+      </Pressable>
+
+      <Text style={{ color: isDarkMode ? "#FFFFFF" : "#000000", marginBottom: 10 }}>
+
+      </Text>
+
+      <Switch
+        value={isDarkMode}
+        onValueChange={() =>
+          setIsDarkMode((previousState) => !previousState)
+        }
+        trackColor={{ false: "#767577", true: "lightblue" }}
+        thumbColor="#f4f3f4"
+      />
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
-/*Zona3: Estilos y posicionamiento*/
+// Zona 3 - Estilos - Personalización de los componentes
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#FFFFFF",
   },
+
+  button: {
+    width: 220,
+    height: 55,
+    backgroundColor: "#2563EB",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginBottom: 25,
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
 });
