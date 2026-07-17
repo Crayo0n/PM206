bbaf8be4e6d5844ad3ea36cb2ba3c2b8f206f90b
Mauricio <mauricio28rd@gmail.com>
2026-07-16 19:15:50 -0600

    Practica 19 Expo Router

---
+++ TercerParcial/usuarioApi/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+import AltaUsuariosScreen from './screens/AltaUsuariosScreen';
+import ConsultaUsuariosScreen from './screens/ConsultaUsuariosScreen';
+
+
+
+export default function App() {
+
+  return(
+
+    <AltaUsuariosScreen></AltaUsuariosScreen>
+
+
+  );
+
+}
+
